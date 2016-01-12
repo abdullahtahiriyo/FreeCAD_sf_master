3fad9ebc4026be63bc86fe28a5b7fd1b8f96d6ae
DeepSOIC <vv.titov@gmail.com>
2016-01-13 01:56:27 +0300

    Sketcher: Hyperbola: fix - remove periodicity correction
    Not needed, since hyperbola is not periodic, unlike circle and ellipse

--- src/Mod/Part/App/Geometry.cpp
+++ src/Mod/Part/App/Geometry.cpp
@@ -2077,10 +2077,6 @@ void GeomArcOfHyperbola::getRange(double& u, double& v, bool emulateCCWXY) const
         if(isReversedInXY()){
             std::swap(u,v);
             u = -u; v = -v;
-            if (v < u)
-                v += 2*M_PI;
-            if (v-u > 2*M_PI)
-                v -= 2*M_PI;
         }
     }
 }
