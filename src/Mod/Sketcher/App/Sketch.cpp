a4ac012947ac26596df10783568d4e15f490a07a
Abdullah Tahiri <abdullah.tahiri.yo@gmail.com>
2020-12-23 07:55:09 +0100

    Sketcher: fix wrong pole parameter initialisation

--- src/Mod/Sketcher/App/Sketch.cpp
+++ src/Mod/Sketcher/App/Sketch.cpp
@@ -1022,8 +1022,8 @@ int Sketch::addBSpline(const Part::GeomBSplineCurve &bspline, bool fixed)
         spoles.push_back(p);
 
         if(!fixed) {
-            param2geoelement.emplace(std::piecewise_construct, std::forward_as_tuple(p.x), std::forward_as_tuple(Geoms.size(), Sketcher::none));
-            param2geoelement.emplace(std::piecewise_construct, std::forward_as_tuple(p.y), std::forward_as_tuple(Geoms.size(), Sketcher::none));
+            param2geoelement.emplace(std::piecewise_construct, std::forward_as_tuple(p.x), std::forward_as_tuple(Geoms.size(), Sketcher::mid));
+            param2geoelement.emplace(std::piecewise_construct, std::forward_as_tuple(p.y), std::forward_as_tuple(Geoms.size(), Sketcher::mid));
         }
     }
 
