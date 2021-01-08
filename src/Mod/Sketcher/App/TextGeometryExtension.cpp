14dcec2739342df8f56bff1d14248dad0e12f157
Abdullah Tahiri <abdullah.tahiri.yo@gmail.com>
2021-01-08 16:31:47 +0100

    Sketcher: TextGeometryExtension framework
    =========================================
    
    Extension intended to bring text along a path defined by a geometry in the Sketcher.

---
+++ src/Mod/Sketcher/App/TextGeometryExtension.cpp
@@ -0,0 +1,83 @@
+/***************************************************************************
+ *   Copyright (c) 2021 Abdullah Tahiri <abdullah.tahiri.yo@gmail.com>     *
+ *                                                                         *
+ *   This file is part of the FreeCAD CAx development system.              *
+ *                                                                         *
+ *   This library is free software; you can redistribute it and/or         *
+ *   modify it under the terms of the GNU Library General Public           *
+ *   License as published by the Free Software Foundation; either          *
+ *   version 2 of the License, or (at your option) any later version.      *
+ *                                                                         *
+ *   This library  is distributed in the hope that it will be useful,      *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU Library General Public License for more details.                  *
+ *                                                                         *
+ *   You should have received a copy of the GNU Library General Public     *
+ *   License along with this library; see the file COPYING.LIB. If not,    *
+ *   write to the Free Software Foundation, Inc., 59 Temple Place,         *
+ *   Suite 330, Boston, MA  02111-1307, USA                                *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "PreCompiled.h"
+
+#include <Base/Writer.h>
+#include <Base/Reader.h>
+#include <Base/Exception.h>
+
+#include "TextGeometryExtension.h"
+
+#include "TextGeometryExtensionPy.h"
+
+using namespace Sketcher;
+
+TYPESYSTEM_SOURCE(Sketcher::TextGeometryExtension,Part::GeometryPersistenceExtension)
+
+std::unique_ptr<Part::GeometryExtension> TextGeometryExtension::copy(void) const
+{
+    auto cpy = std::make_unique<TextGeometryExtension>();
+
+    copyAttributes(cpy.get());
+
+#if defined (__GNUC__) && (__GNUC__ <=4)
+    return std::move(cpy);
+#else
+    return cpy;
+#endif
+}
+
+void TextGeometryExtension::copyAttributes(Part::GeometryExtension * cpy) const
+{
+    Part::GeometryPersistenceExtension::copyAttributes(cpy);
+    static_cast<TextGeometryExtension *>(cpy)->shapeOverride = this->shapeOverride;
+}
+
+void TextGeometryExtension::restoreAttributes(Base::XMLReader &reader)
+{
+    Part::GeometryPersistenceExtension::restoreAttributes(reader);
+    shapeOverride = reader.getAttributeAsInteger("shapeOverride");
+}
+
+void TextGeometryExtension::saveAttributes(Base::Writer &writer) const
+{
+    Part::GeometryPersistenceExtension::saveAttributes(writer);
+    writer.Stream() << "\" shapeOverride=\"" << shapeOverride;
+}
+
+PyObject * TextGeometryExtension::getPyObject(void)
+{
+    return new TextGeometryExtensionPy(new TextGeometryExtension(*this));
+}
+
+TopoDS_Shape TextGeometryExtension::toShape() const
+{
+    if(geometry) {
+        if(shapeOverride)
+            return TopoDS_Shape();
+        else
+            return geometry->toShape();
+    }
+
+    THROWM(Base::ValueError, "TextGeometryExtension does not have valid pointer to the Geometry object.");
+}
---
+++ src/Mod/Sketcher/App/TextGeometryExtensionPyImp.cpp
@@ -0,0 +1,101 @@
+/***************************************************************************
+ *   Copyright (c) 2021 Abdullah Tahiri <abdullah.tahiri.yo@gmail.com>     *
+ *                                                                         *
+ *   This file is part of the FreeCAD CAx development system.              *
+ *                                                                         *
+ *   This library is free software; you can redistribute it and/or         *
+ *   modify it under the terms of the GNU Library General Public           *
+ *   License as published by the Free Software Foundation; either          *
+ *   version 2 of the License, or (at your option) any later version.      *
+ *                                                                         *
+ *   This library  is distributed in the hope that it will be useful,      *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU Library General Public License for more details.                  *
+ *                                                                         *
+ *   You should have received a copy of the GNU Library General Public     *
+ *   License along with this library; see the file COPYING.LIB. If not,    *
+ *   write to the Free Software Foundation, Inc., 59 Temple Place,         *
+ *   Suite 330, Boston, MA  02111-1307, USA                                *
+ *                                                                         *
+ ***************************************************************************/
+
+
+#include "PreCompiled.h"
+
+#include "TextGeometryExtensionPy.h"
+#include "TextGeometryExtensionPy.cpp"
+
+using namespace Sketcher;
+
+// returns a string which represents the object e.g. when printed in python
+std::string TextGeometryExtensionPy::representation(void) const
+{
+    std::stringstream str;
+    bool override = getTextGeometryExtensionPtr()->isShapeOverrideEnabled();
+    str << "<TextGeometryExtension (" ;
+
+    if(getTextGeometryExtensionPtr()->getName().size()>0)
+        str << "\'" << getTextGeometryExtensionPtr()->getName() << "\', ";
+
+    str << (override?"TextEnabled":"TextDisabled") << ") >";
+
+    return str.str();
+}
+
+PyObject *TextGeometryExtensionPy::PyMake(struct _typeobject *, PyObject *, PyObject *)  // Python wrapper
+{
+    // create a new instance of the python object and the Twin object
+    return new TextGeometryExtensionPy(new TextGeometryExtension);
+}
+
+// constructor method
+int TextGeometryExtensionPy::PyInit(PyObject* args, PyObject* /*kwd*/)
+{
+
+    if (PyArg_ParseTuple(args, "")) {
+        // default extension
+        return 0;
+    }
+
+    PyErr_Clear();
+    PyObject * val;
+    if (PyArg_ParseTuple(args, "O!", &PyBool_Type, &val)) {
+        this->getTextGeometryExtensionPtr()->setShapeOverrideEnabled( PyObject_IsTrue(val) ? true : false);
+        return 0;
+    }
+
+    PyErr_Clear();
+    char * pystr;
+    if (PyArg_ParseTuple(args, "O!s",  &PyBool_Type, &val, &pystr)) {
+        this->getTextGeometryExtensionPtr()->setShapeOverrideEnabled( PyObject_IsTrue(val) ? true : false);
+        this->getTextGeometryExtensionPtr()->setName(pystr);
+        return 0;
+    }
+
+    PyErr_SetString(PyExc_TypeError, "TextGeometryExtension constructor accepts:\n"
+    "-- empty parameter list\n"
+    "-- boolean\n"
+    "-- boolean, string\n");
+    return -1;
+}
+
+Py::Boolean TextGeometryExtensionPy::getShapeOverrideEnabled(void) const
+{
+    return Py::Boolean(this->getTextGeometryExtensionPtr()->isShapeOverrideEnabled());
+}
+
+void TextGeometryExtensionPy::setShapeOverrideEnabled(Py::Boolean value)
+{
+    this->getTextGeometryExtensionPtr()->setShapeOverrideEnabled(value);
+}
+
+PyObject *TextGeometryExtensionPy::getCustomAttributes(const char* /*attr*/) const
+{
+    return 0;
+}
+
+int TextGeometryExtensionPy::setCustomAttributes(const char* /*attr*/, PyObject* /*obj*/)
+{
+    return 0;
+}
