7ebdc68239138208a58042e12d3215d53e827fca
Abdullah Tahiri <abdullah.tahiri.yo@gmail.com>
2021-11-14 09:05:43 +0100

    Sketcher: CoinManager Stage 2
    =============================
    
    ViewProviderSketch remove unnecessary headers
    
    CoinManager: add parameter handling class to ViewProviderSketch
    
    Sketcher: Move HDPI resolution responsibility from VPSketch to CoinManager
    
    CoinManager: Move responsibility to create the scenograph for edit mode to CoinManager
    
    ViewProviderSketch: Move rubberband to smart pointer
    
    ViewProviderSketch: Group virtual space member into viewProviderParameters
    
    CoinManager: Move full updateColor responsibility to CoinManager
    
    make detectAndShowPreselection private

--- src/Mod/Sketcher/Gui/CoinManagerParameters.h
+++ src/Mod/Sketcher/Gui/CoinManagerParameters.h
@@ -28,8 +28,16 @@
 # include <Inventor/nodes/SoMaterial.h>
 # include <Inventor/nodes/SoCoordinate3.h>
 # include <Inventor/nodes/SoLineSet.h>
+# include <Inventor/nodes/SoGroup.h>
+# include <Inventor/nodes/SoMarkerSet.h>
+# include <Inventor/nodes/SoText2.h>
+# include <Inventor/nodes/SoPickStyle.h>
+# include <Inventor/nodes/SoTranslation.h>
+# include <Inventor/nodes/SoDrawStyle.h>
 # include <Inventor/SbColor.h>
 #endif  // #ifndef _PreComp_
+
+#include <Gui/Inventor/SmSwitchboard.h>
 
 #include <qstring.h>
 #include <qcolor.h>
@@ -100,15 +108,19 @@ struct DrawingParameters {
     static SbColor ExprBasedConstrDimColor;
     static SbColor DeactivatedConstrDimColor;
 
-    // Rendering font information
-    int coinFontSize;
-
     // Icon colors
     static QColor constrIcoColor;
     static QColor nonDrivingConstrIcoColor;
     static QColor constrIconSelColor;
     static QColor constrIconPreselColor;
     static QColor constrIconDisabledColor;
+
+    // Rendering sizes (also to support HDPI monitors)
+    double pixelScalingFactor = 1.0;
+    int coinFontSize = 17;
+    int constraintIconSize = 15;
+    int markerSize = 7;
+
 };
 
 /** @brief      Struct for storing the nodes that need to be edited to represent a geometry layer
@@ -158,6 +170,42 @@ struct ConstraintParameters {
     bool bShowDimensionalName;
     QString sDimensionalStringFormat;
 };
+
+struct EditModeScenegraphNodes {
+    SoSeparator   *EditRoot;
+    SoMaterial    *PointsMaterials;
+    SoMaterial    *CurvesMaterials;
+    SoMaterial    *RootCrossMaterials;
+    SoMaterial    *EditCurvesMaterials;
+    SoMaterial    *EditMarkersMaterials;
+    SoCoordinate3 *PointsCoordinate;
+    SoCoordinate3 *CurvesCoordinate;
+    SoCoordinate3 *RootCrossCoordinate;
+    SoCoordinate3 *EditCurvesCoordinate;
+    SoCoordinate3 *EditMarkersCoordinate;
+    SoLineSet     *CurveSet;
+    SoLineSet     *RootCrossSet;
+    SoLineSet     *EditCurveSet;
+    SoMarkerSet   *EditMarkerSet;
+    SoMarkerSet   *PointSet;
+
+    SoText2       *textX;
+    SoTranslation *textPos;
+
+    SmSwitchboard *constrGroup;
+    SoGroup       *infoGroup;
+    SoPickStyle   *pickStyleAxes;
+
+    SoDrawStyle * PointsDrawStyle;
+    SoDrawStyle * CurvesDrawStyle;
+    SoDrawStyle * RootCrossDrawStyle;
+    SoDrawStyle * EditCurvesDrawStyle;
+    SoDrawStyle * EditMarkersDrawStyle;
+    SoDrawStyle * ConstraintDrawStyle;
+    SoDrawStyle * InformationDrawStyle;
+};
+
+
 
 } // namespace SketcherGui
 
