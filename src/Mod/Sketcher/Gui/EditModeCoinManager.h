b27068acdb60aa1d1b6db85e66b52333bdef7061
Abdullah Tahiri <abdullah.tahiri.yo@gmail.com>
2021-12-04 12:28:36 +0100

    CoinManager: refactor updateConstraintColor

--- src/Mod/Sketcher/Gui/EditModeCoinManager.h
+++ src/Mod/Sketcher/Gui/EditModeCoinManager.h
@@ -193,7 +193,7 @@ public:
     /** @name update coin colors*/
     //@{
     void updateColor();
-    void updateColor(const GeoList & geolist); // overload to be used with temporal geometry.
+    void updateColor(const GeoListFacade & geolistfacade); // overload to be used with temporal geometry.
     //@}
 
 
