5774f727983b4fe55dfbd9963113eb5a208ea105
Abdullah Tahiri <abdullah.tahiri.yo@gmail.com>
2021-12-06 15:21:33 +0100

    Sketcher: DrawSketchHandler improve documentation

--- src/Mod/Sketcher/Gui/DrawSketchHandler.h
+++ src/Mod/Sketcher/Gui/DrawSketchHandler.h
@@ -39,7 +39,11 @@ namespace SketcherGui {
 
 class ViewProviderSketch;
 
-
+/**
+ * In order to enforce a certain degree of encapsulation and promote a not
+ * too tight coupling, while still allowing well defined collaboration,
+ * DrawSketchHandler accesses ViewProviderSketch via this Attorney class
+ */
 class ViewProviderSketchDrawSketchHandlerAttorney {
 private:
     static inline void setPositionText(ViewProviderSketch &vp, const Base::Vector2d &Pos, const SbString &txt);
@@ -73,6 +77,17 @@ struct AutoConstraint
 /** Handler to create new sketch geometry
   * This class has to be reimplemented to create geometry in the
   * sketcher while its in editing.
+  *
+  * DrawSketchHandler takes over the responsibility of drawing edit temporal curves and
+  * markers necessary to enable visual feedback to the user, as well as the UI interaction during
+  * such edits. This is its exclusive responsibility under the Single Responsibility Principle.
+  *
+  * A plethora of speciliased handlers derive from DrawSketchHandler for each specialised editing (see
+  * for example all the handlers for creation of new geometry). These derived classes do * not * have
+  * direct access to the ViewProviderSketchDrawSketchHandlerAttorney. This is intended to keep coupling
+  * under control. However, generic functionality requiring access to the Attorney can be implemented
+  * in DrawSketchHandler and used from its derived classes by virtue of the inheritance. This promotes a
+  * concentrating the coupling in a single point (and code reuse).
   */
 class SketcherGuiExport DrawSketchHandler
 {
