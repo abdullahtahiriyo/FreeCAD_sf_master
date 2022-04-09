7bdf524b454f09ff82c1f9e80779a744302fad52
Abdullah Tahiri <abdullah.tahiri.yo@gmail.com>
2022-04-09 18:22:11 +0200

    DSH Arc and DSH ArcSlot: Crosshair cursor fixes
    ===============================================
    
    Avoid warning though unreachable in setting Crosshair cursors

--- src/Mod/Sketcher/Gui/CommandCreateGeo.cpp
+++ src/Mod/Sketcher/Gui/CommandCreateGeo.cpp
@@ -4152,7 +4152,7 @@ private:
         else // constructionMethod == DrawSketchHandlerArc::ConstructionMethod::ThreeRim
             return QString::fromLatin1("Sketcher_Pointer_Create_3PointArc");
 
-        return QStringLiteral("");
+        return QStringLiteral("None");
     }
 
 private:
@@ -9449,7 +9449,7 @@ private:
                 return QString::fromLatin1("Sketcher_CreateRectangleSlot");
         }
 
-        return QStringLiteral("");
+        return QStringLiteral("None");
     }
 
 private:
