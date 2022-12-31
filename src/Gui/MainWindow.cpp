442d48987eea2f535643f51badc651e284467a35
Abdullah Tahiri <abdullah.tahiri.yo@gmail.com>
2022-12-31 08:24:17 +0100

    Console/ILogger: Extend to include a notifier field
    ===================================================
    
    Extend Console and Ilogger interface to include a string to identify the notifier, which may be the fullname Document#DocumentObject when available.

--- src/Gui/MainWindow.cpp
+++ src/Gui/MainWindow.cpp
@@ -2135,8 +2135,10 @@ void StatusBarObserver::OnChange(Base::Subject<const char*> &rCaller, const char
     }
 }
 
-void StatusBarObserver::SendLog(const std::string& msg, Base::LogStyle level)
+void StatusBarObserver::SendLog(const std::string& notifiername, const std::string& msg, Base::LogStyle level)
 {
+    (void) notifiername;
+    
     int messageType = -1;
     switch(level){
         case Base::LogStyle::Warning:
