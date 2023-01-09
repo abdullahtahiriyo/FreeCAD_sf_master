d1d35e91a79d65144cb302d37b5b3c794b1d5679
Abdullah Tahiri <abdullah.tahiri.yo@gmail.com>
2023-01-09 19:17:50 +0100

    Sketcher: remove constraint internal alignment

--- src/Mod/Sketcher/Gui/Workbench.cpp
+++ src/Mod/Sketcher/Gui/Workbench.cpp
@@ -324,7 +324,6 @@ inline void SketcherAddWorkbenchConstraints<Gui::MenuItem>(Gui::MenuItem& cons)
             << "Sketcher_ConstrainRadiam"
             << "Sketcher_ConstrainAngle"
             << "Sketcher_ConstrainSnellsLaw"
-            << "Sketcher_ConstrainInternalAlignment"
             << "Separator"
             << "Sketcher_ToggleDrivingConstraint"
             << "Sketcher_ToggleActiveConstraint";
@@ -351,7 +350,6 @@ inline void SketcherAddWorkbenchConstraints<Gui::ToolBarItem>(Gui::ToolBarItem& 
             << "Sketcher_CompConstrainRadDia"
             << "Sketcher_ConstrainAngle"
             // << "Sketcher_ConstrainSnellsLaw" // Rarely used, show only in menu
-            // << "Sketcher_ConstrainInternalAlignment" // This constrain is never used by the user - Do not use precious toolbar space
             << "Separator"
             << "Sketcher_ToggleDrivingConstraint"
             << "Sketcher_ToggleActiveConstraint";
