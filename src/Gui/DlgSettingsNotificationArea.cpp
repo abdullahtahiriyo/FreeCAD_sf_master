fb923f1635c43c60ce2e908f60537f0b4bf53a76
Abdullah Tahiri <abdullah.tahiri.yo@gmail.com>
2023-01-12 14:16:44 +0100

    Gui: Preference Page for NotificationArea

---
+++ src/Gui/DlgSettingsNotificationArea.cpp
@@ -0,0 +1,64 @@
+/***************************************************************************
+ *   Copyright (c) 2023 Abdullah Tahiri <abdullah.tahiri.yo@gmail.com>     *
+ *                                                                         *
+ *   This file is part of the FreeCAD CAx development system.              *
+ *                                                                         *
+ *   This library is free software; you can redistribute it and/or         *
+ *   modify it under the terms of the GNU Library General Public           *
+ *   License as published by the Free Software Foundation; either          *
+ *   version 2 of the License, or (at your option) any later version.      *
+ *                                                                         *
+ *   This library  is distributed in the hope that it will be useful,      *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU Library General Public License for more details.                  *
+ *                                                                         *
+ *   You should have received a copy of the GNU Library General Public     *
+ *   License along with this library; see the file COPYING.LIB. If not,    *
+ *   write to the Free Software Foundation, Inc., 59 Temple Place,         *
+ *   Suite 330, Boston, MA  02111-1307, USA                                *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "PreCompiled.h"
+
+#include "DlgSettingsNotificationArea.h"
+#include "ui_DlgSettingsNotificationArea.h"
+
+
+using namespace Gui::Dialog;
+
+/* TRANSLATOR Gui::Dialog::DlgSettingsNotificationArea */
+
+DlgSettingsNotificationArea::DlgSettingsNotificationArea(QWidget* parent)
+  : PreferencePage(parent)
+  , ui(new Ui_DlgSettingsNotificationArea)
+{
+    ui->setupUi(this);
+}
+
+DlgSettingsNotificationArea::~DlgSettingsNotificationArea()
+{
+}
+
+void DlgSettingsNotificationArea::saveSettings()
+{
+    ui->NotificationAreaEnabled->onSave();
+    ui->NonIntrusiveNotificationsEnabled->onSave();
+}
+
+void DlgSettingsNotificationArea::loadSettings()
+{
+    ui->NotificationAreaEnabled->onRestore();
+    ui->NonIntrusiveNotificationsEnabled->onRestore();
+}
+
+void DlgSettingsNotificationArea::changeEvent(QEvent *e)
+{
+    if (e->type() == QEvent::LanguageChange) {
+        ui->retranslateUi(this);
+    }
+    QWidget::changeEvent(e);
+}
+
+#include "moc_DlgSettingsNotificationArea.cpp"
